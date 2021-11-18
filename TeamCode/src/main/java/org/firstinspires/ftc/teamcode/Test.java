660bb0249523ed8c1bbd3321b88901124fde1799
teky1 <officialteky@gmail.com>
2021-11-18 15:14:39 -0500

    basic motor code ig

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,1 +1,26 @@
-@TeleOp(name="Test")
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+// just runs a motor "only_motor"
+@TeleOp(name="Test", group="Linear Opmode")
+public class TestOpMode extends LinearOpMode {
+
+    private DcMotor motor = null;
+
+    @Override
+    public void runOpMode(){
+        motor  = hardwareMap.get(DcMotor.class, "only_motor");
+        motor.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+
+        while (opModeIsActive()){
+            motor.setPower(1);
+        }
+    }
+}
