aa6389ca8c17d63be5172756f2f772b6a1c1d931
pi-person314 <jadenmengl@gmail.com>
2021-11-18 15:21:12 -0500

    practice

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp
+public class Practice extends LinearOpMode {
+
+    private DcMotor leftMotor = null;
+    private DcMotor rightMotor = null;
+
+    @Override
+    public void runOpMode() {
+        leftMotor = hardwareMap.get(DcMotor.class, "left_motor");
+        rightMotor = hardwareMap.get(DcMotor.class, "right_motor");
+
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+
+        while (opModeIsActive()) {
+
+            double leftPower;
+            double rightPower;
+
+            double drive = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+            leftPower = Range.clip(drive + turn, -1.0, 1.0);
+            rightPower = Range.clip(drive - turn, -1.0, 1.0);
+
+            leftMotor.setPower(leftPower);
+            rightMotor.setPower(rightMotor);
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,2 +1,1 @@
 @TeleOp(name="Test")
-hello
