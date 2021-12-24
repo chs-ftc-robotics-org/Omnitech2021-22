f61ccc0173aedc2b0bdbafd29db38586d9bd74c6
pi-person314 <jadenmengl@gmail.com>
2021-12-24 11:33:58 -0500

    intake subsystem

---
+++ TeamCode/src/main/java/omnitech/subsystems/Intake.java
@@ -0,0 +1,39 @@
+package omnitech.subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import omnitech.Subsystem;
+
+public class Intake implements Subsystem {
+
+    public static final boolean active = false;
+
+    // will make this more descriptive once we have an actual design
+    public DcMotor intakeMotor;
+
+    private double intakePower = 0.25;
+
+    @Override
+    public void initialize(LinearOpMode opMode) {
+        intakeMotor = opMode.hardwareMap.get(DcMotor.class,"intake_motor");
+        intakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    @Override
+    public boolean active() { return active; }
+
+    public void setIntakePower(double power) { intakePower = power; }
+
+    public void intakeMove(boolean moving) {
+        if (moving) {
+            intakeMotor.setPower(intakePower);
+        }
+        else {
+            intakeMotor.setPower(0.0);
+
+        }
+    }
+}
