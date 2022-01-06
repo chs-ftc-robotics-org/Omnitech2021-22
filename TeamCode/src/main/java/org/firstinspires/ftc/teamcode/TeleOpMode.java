3a43e0dba664d01a0aead00f061bffebd889f951
pi-person314 <jadenmengl@gmail.com>
2022-01-06 15:27:26 -0500

    created main teleopmode with drivetrain and carousel code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import omnitech.OurRobot;
+import omnitech.subsystems.Drivetrain;
+
+@TeleOp
+public class TeleOpMode extends LinearOpMode {
+
+    private OurRobot robot;
+
+    @Override
+    public void runOpMode() {
+
+        telemetry.addData("State", "Initializing...");
+        telemetry.update();
+
+        robot = new OurRobot();
+        robot.initialize(this);
+
+        telemetry.addData("State", "Ready to start...");
+        telemetry.update();
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            // drivetrain
+            double drivePower = gamepad1.right_trigger-gamepad1.left_trigger;
+            double turnAmt = gamepad1.left_stick_x;
+            double strafeX = gamepad1.right_stick_x;
+            double strafeY = gamepad1.right_stick_y;
+
+            robot.drivetrain.power(Drivetrain.Movement.POV, turnAmt, drivePower);
+            robot.drivetrain.power(Drivetrain.Movement.STRAFE, strafeX, strafeY);
+
+            // carousel
+//            if (gamepad1.a) {
+//                robot.carousel.turn(true);
+//            }
+//            else {
+//                robot.carousel.turn(false);
+//            }
+
+            // intake (probably use a button to turn on and off)
+            // outtake (probably use left and right bumper)
+        }
+    }
+}
