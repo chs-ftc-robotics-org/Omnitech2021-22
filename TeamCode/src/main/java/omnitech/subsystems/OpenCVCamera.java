8a3045a2f298aebf6dc2e1feaedb8a8860a30be0
ijax1 <ianshimjackson@gmail.com>
2022-01-09 20:28:33 -0500

    Setting up openCV webcam code

---
+++ TeamCode/src/main/java/omnitech/subsystems/OpenCVCamera.java
@@ -0,0 +1,16 @@
+package omnitech.subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import omnitech.Subsystem;
+
+public class OpenCVCamera implements Subsystem {
+
+    public void initialize(LinearOpMode opMode) {
+
+    }
+
+    public boolean active() {
+        return false;
+    }
+}
