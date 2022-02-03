091ac97c5b5c837fcf32f62911f6379c10ad5f0b
ijax <ianshimjackson@gmail.com>
2022-02-02 21:39:55 -0500

    motors reversed?

--- TeamCode/src/main/java/omnitech/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/omnitech/subsystems/Drivetrain.java
@@ -39,8 +39,8 @@ public class Drivetrain implements Subsystem {
 
         rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
         rightRear.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftRear.setDirection(DcMotorSimple.Direction.FORWARD);
 
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
